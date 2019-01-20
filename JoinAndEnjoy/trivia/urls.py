27cd7799c1f6b75acd70f98c2140234664d2268c
Tali <tali693@gmail.com>
2019-01-20 20:21:18 +0200

    player answers

--- JoinAndEnjoy/trivia/urls.py
+++ JoinAndEnjoy/trivia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_screen, name='main_screen'),
+    path('main_screen', views.main_screen, name='main_screen'),
+    path('player_welcome', views.player_welcome, name='player_welcome'),
+    path('player_answers', views.player_answers, name='player_answers'),
 ]
+
--- JoinAndEnjoy/trivia/views.py
+++ JoinAndEnjoy/trivia/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 def main_screen(request):
     return render(request, "trivia/main_screen.html")
 
+
 def player_welcome(request):
     return render(request, "trivia/player_welcome.html")
+
 
 def player_answers(request):
     return render(request, "trivia/player_answers.html")
