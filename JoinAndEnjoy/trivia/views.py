4408da737cb9ed70d3cbeee7889d8f35df36dc01
chanismile <37192892+chanismile@users.noreply.github.com>
2019-01-20 19:00:28 +0200

    fix the commits

---
+++ JoinAndEnjoy/trivia/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def main_screen(request):
+    return render(request, "trivia/main_screen.html")
+
+def player_welcome(request):
+    return render(request, "trivia/player_welcome.html")
+
+def player_answers(request):
+    return render(request, "trivia/player_answers.html")
