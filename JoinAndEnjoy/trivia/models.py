483d9be6bac8a8d8a77dc23ce430f84bbcc08664
Giti Vebber <giti.vebber03@gmail.com>
2019-01-21 13:59:03 +0200

    player_welcom.html

--- JoinAndEnjoy/trivia/models.py
+++ JoinAndEnjoy/trivia/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    # id_player=models.IntegerField(default=0)
+    name = models.CharField(max_length=200)
+    # score= models.IntegerField(default=0)
--- JoinAndEnjoy/trivia/views.py
+++ JoinAndEnjoy/trivia/views.py
@@ -1,13 +1,41 @@
+from django import forms
 from django.shortcuts import render
+
+
+
+# class ExpenseForm(forms.ModelForm):
+#     class Meta:
+#         model = Expense
+#         fields = "__all__"
+from trivia.models import Player
 
 
 def main_screen(request):
     return render(request, "trivia/main_screen.html")
 
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = "__all__"
+
 
 def player_welcome(request):
-    return render(request, "trivia/player_welcome.html")
+    if request.method == "POST":
+         form = ExpenseForm(request.POST)
+         # assert False, form.cleaned_data
+         if form.is_valid():
+             form.instance.save()
 
+        # Player.objects.create(name=request.POST['player_name'],
+        #                     score=0)
+            # return redirect(form.instance)
+    # return render(request, "trivia/player_welcome.html")
+    else:
+        form = ExpenseForm()
+    return render(request, "trivia/player_welcome.html", {
+        'form': form,
+    })
 
 def player_answers(request):
     return render(request, "trivia/player_answers.html")
+
