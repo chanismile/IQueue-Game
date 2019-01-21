537d7bddb8eef1077adb2b46fcd1b9198d85e65b
Tali <tali693@gmail.com>
2019-01-21 22:53:05 +0200

    update views and htmls

--- JoinAndEnjoy/trivia/urls.py
+++ JoinAndEnjoy/trivia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('main_screen', views.main_screen, name='main_screen'),
     path('player_welcome', views.player_welcome, name='player_welcome'),
-    path('player_answers', views.player_answers, name='player_answers'),
+    path('player_choices', views.player_choices, name='player_choices'),
 ]
--- JoinAndEnjoy/trivia/views.py
+++ JoinAndEnjoy/trivia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def main_screen(request):
@@ -6,8 +6,43 @@ def main_screen(request):
 
 
 def player_welcome(request):
+    if request.method == "POST":
+        p = Player.objects.create(name=request.POST['name'])
+        request.session['player_id'] = p.id
+        return redirect('player_choices')
+
     return render(request, "trivia/player_welcome.html")
 
 
-def player_answers(request):
-    return render(request, "trivia/player_answers.html")
+def player_choices(request):
+
+    # if time isn't over
+    if True:
+        active = ''#
+        if request.method == "POST":
+            request.session['player_choice'] = request.POST.get('choice')
+            # return redirect("player_choices")
+        elif request.POST.get('choice'):
+            active = 'active_wait'
+
+        # else:
+        #     return redirect("player_choices")
+
+        # render(request, "trivia/player_choices", active_view)
+        return redirect("player_choices")
+    else:
+        cq = CurrentQuestion.objects.get(pk=1)
+        q = Question.objects.get(pk=cq.question)
+        correct_answer = q.correct_choice
+
+        if request.POST.get('choice') == correct_answer:
+            active = 'active_correct'
+        else:
+            active = 'active_incorrect'
+        active_view = {
+            'active': active
+        }
+        return render(request, "trivia/player_choices.html", active_view)
+
+    # return render(request, "trivia/player_choices.html", active_view)
+
