9d2813add881a83d73e0c574b3b52248e543b333
chanismile <37192892+chanismile@users.noreply.github.com>
2019-01-21 13:20:42 +0200

    players model+ update player_welcome.html+ update view/player_welcone

--- JoinAndEnjoy/trivia/models.py
+++ JoinAndEnjoy/trivia/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Player(models.Model):
+   # id_player=models.IntegerField(default=0)
+   name = models.CharField(max_length=200)
+   # score= models.IntegerField(default=0)
+
+   def __str__(self):
+       return self.name
+
+   def get_absolute_url(self):
+       return reverse("Player:detail", args=(self.id,))
--- JoinAndEnjoy/trivia/views.py
+++ JoinAndEnjoy/trivia/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django import forms
+from django.views.generic import CreateView
+from django.shortcuts import render, get_object_or_404, redirect
+from trivia.models import Player
 
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = "__all__"
 
 def main_screen(request):
     return render(request, "trivia/main_screen.html")
 
 
 def player_welcome(request):
-    return render(request, "trivia/player_welcome.html")
+    if request.method == "POST":
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            form.instance.save()
+            return redirect(form.instance)
+    else:
+        form = PlayerForm()
+
+    return render(request, "trivia/player_welcome.html", {
+        'form': form,
+    })
 
 
 def player_answers(request):
